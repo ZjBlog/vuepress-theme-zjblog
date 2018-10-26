7f26461ad46d197fda2d5d6c8d73a1fcffc3a475
zhangjunTracy <13552960783@163.com>
2018-10-26 20:19:15 +0800

    404页面

--- NotFound.vue
+++ NotFound.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="theme-container">
-    <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">back to the homepage</router-link>
-    </div>
-  </div>
+		<div class="container">
+			<img class="bg" src="./assets/404.png"/>
+			<div class="btn">
+				<a href="/" class="goindex">返回首页</a>
+				<a href="/" class="lx">随便看看</a>
+				<div style="clear:both;"></div>
+			</div>
+		</div>
 </template>
 
 <script>
-const msgs = [
-  `The page went on flight 404.`,
-  `How did you come here?`,
-  `It seems that there is a problem, it is being repaired.`,
-  `Emmm, the page was hit by a two-way foil.`
-]
-
 export default {
   methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
   }
 }
 </script>
+<style scoped>
+*{ padding:0;margin:0;box-sizing:border-box;font-family:"微软雅黑";}
+body,html{width:100%;height:100%;}
+.container{max-width:90%;margin:0 auto;padding:80px 0px; }
+img{-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;}
+.bg{display:block;max-width:100%;margin:0px auto;margin:40px auto;}
+.btn{width:400px;margin:0 auto;max-width:100%;margin-top:40px;}
+.btn a{float:left;text-decoration:none;width:46.5%;border:1px solid #5298ff;background:#5298ff;color:#FFF;display:block;height:46px; line-height:44px;text-align:center;font-size:16px;border-radius:3px;overflow:hidden;}
+.btn .goindex{margin-right:7%;}
+.btn .lx{border: 1px solid #d8d8d8;background:#ffffff;color:#8c8c8c;}
+@media screen and (max-width: 500px){
+.btn{ width:85%; }
+.btn a{ width:100%; font-size:15px; height:42px; line-height:42px; }
+.btn .goindex{ margin-right:0; margin-bottom:20px; }
+}
+</style>
+
