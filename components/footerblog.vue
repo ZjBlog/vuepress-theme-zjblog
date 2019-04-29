327d4a63f39df9f61ddcc9e9fb7f924af469d0bc
zj <13552960783@163.com>
2019-04-29 09:36:34 +0800

    fix bug

--- components/footerblog.vue
+++ components/footerblog.vue
@@ -30,7 +30,7 @@ export default {
   mounted () {
     aa()
     if(this.$site.themeConfig.cnzzUrl){
-      let web_id=$site.themeConfig.cnzzUrl.substring($site.themeConfig.cnzzUrl.lastIndexOf('=')+1)
+      let web_id=this.$site.themeConfig.cnzzUrl.substring(this.$site.themeConfig.cnzzUrl.lastIndexOf('=')+1)
       this.url='https://new.cnzz.com/v1/login.php?siteid=' + web_id
     }
   },
